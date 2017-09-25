a720d8be9fb82397a55bf04937d94b97fbbba556
sourysanis <syrous.sina@gmail.com>
2017-09-25 15:52:15 +0200

    Compliment
    Initial commit to give remaining components

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,44 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'In-N-Out Burger',
+      'I prefer Shake Shack!',
+      'https://upload.wikimedia.org/wikipedia/commons/f/ff/In-N-Out_Burger_cheeseburgers_and_fries.jpg',
+      [
+        new Ingredient('Meat', 2),
+        new Ingredient('Fries', 20),
+        new Ingredient('Buns', 1)
+      ]),
+    new Recipe('Churros',
+      'Heaven on eart!',
+      'https://cdn.pixabay.com/photo/2017/03/30/15/47/churros-2188871_1280.jpg',
+      [
+        new Ingredient('Flour', 2),
+        new Ingredient('Sugar', 2),
+        new Ingredient('Frying oil', 2)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
